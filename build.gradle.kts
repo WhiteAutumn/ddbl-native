c53cea80652f079cf9d8f4b84e970503d0c85cd1
White Autumn <white@autumn.dev>
2025-05-12 20:19:43 +0200

    update reflection config with mixing & agent generated config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
+import java.net.ServerSocket
 import java.net.URLClassLoader
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.jar.JarFile
 
 plugins {
@@ -20,7 +23,7 @@ version = "0.0.2"
 
 val dynamoDbVersion = "2.6.0"
 
-val main: String =
+val mainClassName: String =
 	(findProperty("main") as String?)
 		?: "dev.autumn.ddblnative.MainKt"
 
@@ -28,11 +31,29 @@ val march: String =
 	(findProperty("march") as String?)
 		?: "native"
 
+sourceSets {
+	test {
+		kotlin.srcDirs(emptyList<String>())
+		resources.srcDirs(emptyList<String>())
+	}
+
+	create("exercise") {
+		kotlin.srcDir("src/exercise/kotlin")
+		resources.srcDir("src/exercise/resources")
+	}
+}
+
+kotlin {
+	jvmToolchain(21)
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+	val exerciseImplementation by configurations.getting
+
 	implementation("com.amazonaws:DynamoDBLocal:$dynamoDbVersion") {
 		exclude(group = "org.apache.logging.log4j", module = "log4j-core")
 		exclude(group = "org.apache.logging.log4j", module = "log4j-slf4j2-impl")
@@ -45,17 +66,15 @@ dependencies {
 
 	//runtimeOnly("org.slf4j:slf4j-nop:1.7.36")
 	runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
-}
 
-kotlin {
-	jvmToolchain(21)
+	exerciseImplementation("aws.sdk.kotlin:dynamodb:1.4.81")
 }
 
 graalvmNative {
 	binaries {
 		named("main") {
 			imageName.set("${project.name}-$dynamoDbVersion-${project.version}")
-			mainClass.set(main)
+			mainClass.set(mainClassName)
 
 			configurationFileDirectories.from(layout.buildDirectory.dir("native/config"))
 
@@ -71,48 +90,23 @@ graalvmNative {
 }
 
 tasks.register("generateReflectConfig") {
+	dependsOn("classes", "exerciseClasses")
+
 	val outFile = layout.buildDirectory.file("native/config/reflect-config.json")
 	outputs.file(outFile)
+
+	val agentResultDir = project.layout.buildDirectory.dir("native/agent-result").get().asFile
+	agentResultDir.deleteRecursively()
+	agentResultDir.mkdirs()
 
 	doLast {
 		val json = Json { prettyPrint = true }
 
 		val reflectConfig = buildJsonArray {
-			addJsonObject {
-				put("name", "org.apache.logging.slf4j.SLF4JLoggerContextFactory")
-				putJsonArray("methods") {
-					addJsonObject {
-						put("name", "<init>")
-						putJsonArray("parameterTypes") {}
-					}
-				}
-			}
-
-			addJsonObject {
-				put("name", "org.apache.logging.log4j.message.DefaultFlowMessageFactory")
-				putJsonArray("methods") {
-					addJsonObject {
-						put("name", "<init>")
-						putJsonArray("parameterTypes") {}
-					}
-				}
-			}
-
-			addJsonObject {
-				put("name", "org.apache.logging.log4j.message.ParameterizedMessageFactory")
-				putJsonArray("methods") {
-					addJsonObject {
-						put("name", "<init>")
-						putJsonArray("parameterTypes") {}
-					}
-				}
-			}
+			val reflectionClasses = mutableSetOf<String>()
 
 			val urls = sourceSets.main.get().runtimeClasspath.map { it.toURI().toURL() }
 			val loader = URLClassLoader.newInstance(urls.toTypedArray())
-
-			val awsRequest = loader.loadClass("com.amazonaws.AmazonWebServiceRequest")
-			val awsResult  = loader.loadClass("com.amazonaws.AmazonWebServiceResult")
 
 			val modelClasses: List<String> =
 				sourceSets.main.get().runtimeClasspath
@@ -133,16 +127,70 @@ tasks.register("generateReflectConfig") {
 					.sorted()
 
 			for (name in modelClasses) {
-				val cls = runCatching { loader.loadClass(name) }.getOrNull()
+				addJsonObject {
+					reflectionClasses += name
+					put("name", name)
+					put("allDeclaredConstructors", true)
+					put("allDeclaredMethods", true)
+					put("allDeclaredFields", true)
+				}
+			}
+
+			val objectMapperClass = loader.loadClass(
+				"com.amazonaws.services.dynamodbv2.local.shared.mapper.DynamoDBObjectMapper"
+			)
+
+			val objectMapperMixinClasses = objectMapperClass.declaredClasses
+				.filter { it.simpleName.endsWith("MixIn") }
+				.map { it.name }
+				.distinct()
+				.sorted()
+
+			for (name in objectMapperMixinClasses) {
+				addJsonObject {
+					reflectionClasses += name
+					put("name", name)
+					put("allDeclaredConstructors", true)
+					put("allDeclaredMethods", true)
+					put("allDeclaredFields", true)
+				}
+			}
+
+			val main by sourceSets.getting
+			val exercise by sourceSets.getting
+
+			val port = ServerSocket(0).use { it.localPort }
+
+			val runtimeClassPath = main.runtimeClasspath
+				.joinToString(File.pathSeparator) { it.absolutePath }
+
+			val serverProcess = ProcessBuilder(
+				"java",
+				"-agentlib:native-image-agent=config-output-dir=$agentResultDir",
+				"-cp", runtimeClassPath,
+				mainClassName,
+				"-inMemory", "-sharedDb", "-port", port.toString()
+			)
+				.start()
+
+			project.javaexec {
+				classpath = exercise.runtimeClasspath
+				mainClass.set("dev.autumn.ddblnative.ExerciseKt")
+				args("http://127.0.0.1:$port")
+			}
+
+			serverProcess.destroy()
+			serverProcess.waitFor()
+
+
+			val agentResultText = String(Files.readAllBytes(Paths.get("$agentResultDir/reflect-config.json")))
+			for (element in json.parseToJsonElement(agentResultText).jsonArray) {
+				val name = element.jsonObject["name"]?.jsonPrimitive?.content
 					?: continue
 
-				if (awsRequest.isAssignableFrom(cls) || awsResult.isAssignableFrom(cls)) {
-					addJsonObject {
-						put("name", name)
-						put("allDeclaredConstructors", true)
-						put("allDeclaredMethods", true)
-						put("allDeclaredFields", true)
-					}
+				if (name !in reflectionClasses) {
+					reflectionClasses += name
+					add(element)
 				}
 			}
 		}
@@ -157,4 +205,3 @@ tasks.register("generateReflectConfig") {
 tasks.named("nativeCompile") {
 	dependsOn("generateReflectConfig")
 }
-
---
+++ src/exercise/kotlin/dev/autumn/ddblnative/Exercise.kt
@@ -0,0 +1,84 @@
+package dev.autumn.ddblnative
+
+import aws.sdk.kotlin.services.dynamodb.DynamoDbClient
+import aws.sdk.kotlin.services.dynamodb.createTable
+import aws.sdk.kotlin.services.dynamodb.describeTable
+import aws.sdk.kotlin.services.dynamodb.getItem
+import aws.sdk.kotlin.services.dynamodb.listTables
+import aws.sdk.kotlin.services.dynamodb.model.AttributeDefinition
+import aws.sdk.kotlin.services.dynamodb.model.AttributeValue
+import aws.sdk.kotlin.services.dynamodb.model.BillingMode
+import aws.sdk.kotlin.services.dynamodb.model.KeySchemaElement
+import aws.sdk.kotlin.services.dynamodb.model.KeyType
+import aws.sdk.kotlin.services.dynamodb.model.ScalarAttributeType
+import aws.sdk.kotlin.services.dynamodb.model.TableStatus
+import aws.sdk.kotlin.services.dynamodb.putItem
+import aws.smithy.kotlin.runtime.net.url.Url
+import kotlinx.coroutines.delay
+import kotlin.system.exitProcess
+
+suspend fun main(args: Array<String>) {
+	val (endpoint) = args
+
+	val dynamo = DynamoDbClient {
+		region = "us-east-1"
+		endpointUrl = Url.parse(endpoint)
+	}
+
+	while (true) {
+		try {
+			dynamo.listTables()
+			break
+		}
+		catch (_: Exception) {
+			delay(250)
+		}
+	}
+
+	dynamo.createTable {
+		tableName = "test-table"
+		billingMode = BillingMode.PayPerRequest
+
+		attributeDefinitions = listOf(
+			AttributeDefinition {
+				attributeName = "id"
+				attributeType = ScalarAttributeType.S
+			}
+		)
+		keySchema = listOf(
+			KeySchemaElement {
+				attributeName = "id"
+				keyType = KeyType.Hash
+			}
+		)
+	}
+
+	while (true) {
+		val result = dynamo.describeTable {
+			tableName = "test-table"
+		}
+
+		if (result.table?.tableStatus == TableStatus.Active) {
+			break
+		}
+
+		delay(250)
+	}
+
+	dynamo.putItem {
+		tableName = "test-table"
+		item = mapOf(
+			"id" to AttributeValue.S("1"),
+			"name" to AttributeValue.S("test")
+		)
+	}
+
+	dynamo.getItem {
+		tableName = "test-table"
+		key = mapOf(
+			"id" to AttributeValue.S("1")
+		)
+	}
+
+	exitProcess(0)
+}
